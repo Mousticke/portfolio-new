7625556148377519324148074f685708c005925a
Akim Benchiha <akim.benchiha@gmail.com>
2020-09-26 01:53:31 +0200

    Add places in config

--- src/config/mapData.js
+++ src/config/mapData.js
@@ -5,11 +5,237 @@ const mapData = [
       code: 'FR',
       name: 'France',
     },
-    places: [{
+    places: [
+      {
         id: '0',
         city: 'Fagnières',
         type: 'birth_place',
-        
-    }],
+        lat: 20,
+        lng: 20,
+      },
+      {
+        id: '1',
+        city: 'Châlons en Champagne',
+        type: 'school_life',
+        lat: 20,
+        lng: 20,
+      },
+      {
+        id: '2',
+        city: 'Châlons en Champagne',
+        type: 'previousWork_place',
+        lat: 20,
+        lng: 20,
+      },
+      {
+        id: '3',
+        city: 'Lille',
+        type: 'school_life',
+        lat: 20,
+        lng: 20,
+      },
+      {
+        id: '4',
+        city: 'Lille',
+        type: 'previousHome_place',
+        lat: 20,
+        lng: 20,
+      },
+      {
+        id: '5',
+        city: 'Lille',
+        type: 'previousWork_place',
+        lat: 30,
+        lng: 40,
+      },
+      {
+        id: '6',
+        city: 'Perpignan',
+        type: 'holidays',
+        lat: 20,
+        lng: 20,
+      },
+      {
+        id: '7',
+        city: 'Paris',
+        type: 'holidays',
+        lat: 20,
+        lng: 20,
+      },
+    ],
+  },
+
+  {
+    id: '2',
+    country: {
+      code: 'LU',
+      name: 'Luxembourg',
+    },
+    places: [
+      {
+        id: '8',
+        city: 'Luxembourg',
+        type: 'currentWork_place',
+        lat: 20,
+        lng: 20,
+      },
+      {
+        id: '9',
+        city: 'Luxembourg',
+        type: 'current_location',
+        lat: 20,
+        lng: 20,
+      },
+    ],
+  },
+
+  {
+    id: '3',
+    country: {
+      code: 'SG',
+      name: 'Singapore',
+    },
+    places: [
+      {
+        id: '10',
+        city: 'Tao Payoh',
+        type: 'previousHome_place',
+        lat: 20,
+        lng: 20,
+      },
+      {
+        id: '11',
+        city: 'Kallang',
+        type: 'previousWork_place',
+        lat: 20,
+        lng: 20,
+      },
+      {
+        id: '12',
+        city: 'Marina Bay Sand',
+        type: 'holidays',
+        lat: 20,
+        lng: 20,
+      },
+    ],
+  },
+
+  {
+    id: '4',
+    country: {
+      code: 'DE',
+      name: 'Germany',
+    },
+    places: [
+      {
+        id: '13',
+        city: 'Heidelberg',
+        type: 'holidays',
+        lat: 20,
+        lng: 20,
+      },
+      {
+        id: '14',
+        city: 'Frankfurt',
+        type: 'holidays',
+        lat: 20,
+        lng: 20,
+      },
+      {
+        id: '15',
+        city: 'Trier',
+        type: 'holidays',
+        lat: 20,
+        lng: 20,
+      },
+      {
+        id: '16',
+        city: 'Köln',
+        type: 'holidays',
+        lat: 20,
+        lng: 20,
+      },
+      {
+        id: '17',
+        city: 'Saarbrucken',
+        type: 'holidays',
+        lat: 20,
+        lng: 20,
+      },
+    ],
+  },
+
+  {
+    id: '5',
+    country: {
+      code: 'ES',
+      name: 'Spain',
+    },
+    places: [
+      {
+        id: '18',
+        city: 'Barcelona',
+        type: 'holidays',
+        lat: 20,
+        lng: 20,
+      },
+      {
+        id: '19',
+        city: 'Madrid',
+        type: 'holidays',
+        lat: 20,
+        lng: 20,
+      },
+    ],
+  },
+
+  {
+    id: '6',
+    country: {
+      code: 'PT',
+      name: 'Portugal',
+    },
+    places: [
+      {
+        id: '19',
+        city: 'Lisbon',
+        type: 'holidays',
+        lat: 20,
+        lng: 20,
+      },
+      {
+        id: '20',
+        city: 'Porto',
+        type: 'holidays',
+        lat: 20,
+        lng: 20,
+      },
+    ],
+  },
+
+  {
+    id: '7',
+    country: {
+      code: 'EP',
+      name: 'Egypt',
+    },
+    places: [
+      {
+        id: '21',
+        city: 'Sharmel Cheik',
+        type: 'holidays',
+        lat: 20,
+        lng: 20,
+      },
+      {
+        id: '22',
+        city: 'Le Caire',
+        type: 'holidays',
+        lat: 20,
+        lng: 20,
+      },
+    ],
   },
 ]
+
+export default mapData
