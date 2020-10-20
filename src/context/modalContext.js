ca534a84f44ac95b86236e8bc0e536bb83c7d9ba
Akim Benchiha <akim.benchiha@gmail.com>
2020-10-20 11:48:40 +0200

    Security update for yarn package and props validation for modal provider

--- src/context/modalContext.js
+++ src/context/modalContext.js
@@ -1,11 +1,16 @@
 import React, { createContext } from 'react'
 import useModal from '@hooks/useModal'
+import PropTypes from 'prop-types'
 
 const ModalContext = createContext()
 
 const ModalProvider = ({ children }) => {
   const { modal, handleModal, modalContent } = useModal()
   return <ModalContext.Provider value={{ modal, handleModal, modalContent }}>{children}</ModalContext.Provider>
+}
+
+ModalProvider.propTypes = {
+  children: PropTypes.node.isRequired,
 }
 
 export { ModalContext, ModalProvider }
